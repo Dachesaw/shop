2d936add0b4e4c7ba01029844323755d0cf0712d
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-04-28 03:06:50 +0300

    add list in nav and link

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,18 @@
 const NavBar = () => {
     return ( 
         <nav>
-            
+            <ul>
+                <li><a href="">О Нас</a></li>
+                <li><a href="">Магазины</a></li>
+                <li><a href="">Оплата и Доставка</a></li>
+                <li><a href="">Статус заказа</a></li>
+                <li><a href="">Возврат</a></li>
+                <li><a href="">Контакты</a></li>
+            </ul>
+            <ul>
+                <li><a href="">Москва</a></li>
+                <li><a href="tel:+7(812)834-84-88">+7 (812) 834-84-88</a></li>
+            </ul>
         </nav>
      );
 }
