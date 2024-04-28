dd64a924c65c181a32dcf5348da043da533f7671
Boris <boriskoznev@gmail.com>
2024-04-28 03:12:14 +0300

    add Footer in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Footer from './Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
-  
+
   return (
     <div className='App'>
-
+      <Footer />
     </div>
   )
 }
