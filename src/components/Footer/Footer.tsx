71f87ebf70edf2c192282c80f3577725525a3f62
Boris <boriskoznev@gmail.com>
2024-04-28 03:03:05 +0300

    add Footer folder

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <section>
+            <div>
+                <h3>Hiii</h3>
+                <p>aboba</p>
+            </div>
+        </section>
+    )
+}
