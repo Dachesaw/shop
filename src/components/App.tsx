b79b874ab91ba6db9ea56861580756c86dbaf48e
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-04-28 03:11:03 +0300

    import NavBar in App and connect in div

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import NavBar from './NavBar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
   
   return (
     <div className='App'>
-
+    <NavBar/>
     </div>
   )
 }
