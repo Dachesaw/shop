37fe29c69415d324372f4cbbb8b13fd71a3caabd
kinokorain <boriskoznev@gmail.com>
2024-04-30 16:37:50 +0300

    add input in footer, fixed margins in footer, add accent color variable

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,51 @@
-import burger from '../../assets/burger.svg'
-import favorite from '../../assets/favorite.svg'
-import login from '../../assets/login.svg'
-import shop_basket from '../../assets/shop_basket.svg'
-import search from '../../assets/search.svg'
+import burger from "../../assets/burger.svg";
+import favorite from "../../assets/favorite.svg";
+import login from "../../assets/login.svg";
+import shop_basket from "../../assets/shop_basket.svg";
+import search from "../../assets/search.svg";
 
-import "./Header.css"
+import "./Header.css";
 
 const Header = () => {
-    return ( 
+    return (
         <header>
             <ul>
-                <li><button className='burger_button'><img src={burger}/>КАТАЛОГ</button></li>
+                <li>
+                    <button className="burger_button">
+                        <img src={burger} />
+                        КАТАЛОГ
+                    </button>
+                </li>
                 <li>
                     <form>
-                        <input type="text"  name="text" className="search" placeholder='Поиск товара'/>
-                        <button type="submit" className='button_search'><img src={search} alt="" /></button>
+                        <input
+                            type="text"
+                            name="text"
+                            className="search"
+                            placeholder="Поиск товара"
+                        />
+                        <button type="submit" className="button_search">
+                            <img src={search} alt="" />
+                        </button>
                     </form>
                 </li>
             </ul>
-            <ul className='ul_2'>
-                <li><img src={favorite} alt="" width={25} /><a href="">Избранное</a></li>
-                <li><img src={shop_basket} alt="" width={25}/><a href="">Корзина</a></li>
-                <li><img src={login} alt="" width={25}/><a href="">Войти</a></li>
+            <ul className="ul_2">
+                <li>
+                    <img src={favorite} alt="" width={25} />
+                    <a href="">Избранное</a>
+                </li>
+                <li>
+                    <img src={shop_basket} alt="" width={25} />
+                    <a href="">Корзина</a>
+                </li>
+                <li>
+                    <img src={login} alt="" width={25} />
+                    <a href="">Войти</a>
+                </li>
             </ul>
-            
         </header>
-     );
-}
- 
+    );
+};
+
 export default Header;
