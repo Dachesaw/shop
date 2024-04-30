36383cbf4952bd3aa1b74bf05d91a9ca0f50f605
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-04-30 19:37:12 +0300

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,22 +11,12 @@ const Header = () => {
         <header>
             <ul>
                 <li>
-                    <button className="burger_button">
-                        <img src={burger} />
-                        КАТАЛОГ
-                    </button>
+                    <button className="burger_button"><img src={burger} />КАТАЛОГ</button>
                 </li>
                 <li>
                     <form>
-                        <input
-                            type="text"
-                            name="text"
-                            className="search"
-                            placeholder="Поиск товара"
-                        />
-                        <button type="submit" className="button_search">
-                            <img src={search} alt="" />
-                        </button>
+                        <input type="text"name="text"className="search"placeholder="Поиск товара"/>
+                        <button type="submit" className="button_search"><img src={search} alt="" /></button>
                     </form>
                 </li>
             </ul>
