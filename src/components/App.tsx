40691d828744f9e17be3489b0c0742c310562b51
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-04-30 19:05:43 +0300

    add div classname = category

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import NavBar from './NavBar/NavBar'
 import Footer from './Footer/Footer'
 import Header from './Header/Header'
+import MainPage from './MainPage/MainPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <div className='App'>
       <NavBar />
       <Header />
+      <MainPage />
       <Footer />
     </div>
   )
