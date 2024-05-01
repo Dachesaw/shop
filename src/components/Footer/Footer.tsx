099d0bfed23379531e26149bb5a54caecf63f0b7
kinokorain <boriskoznev@gmail.com>
2024-05-01 17:27:51 +0300

    finished Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,18 @@
 import phoneIcon from "../../assets/tel.svg";
 import mailIcon from "../../assets/email.svg";
+
+import facebook_logo from "../../assets/social-networks/facebook-logo.svg";
+import tg_logo from "../../assets/social-networks/tg-logo.svg";
+import insta_logo from "../../assets/social-networks/insta-logo.svg";
+import ok_logo from "../../assets/social-networks/ok-logo.svg";
+import vk_logo from "../../assets/social-networks/vk-logo.svg";
+import yt_logo from "../../assets/social-networks/yt-logo.svg";
+
+import visa from "../../assets/payment-methods/visa.svg";
+import mastercard from "../../assets/payment-methods/mastercard.svg";
+import mir from "../../assets/payment-methods/mir.svg";
+import gpay from "../../assets/payment-methods/gpay.svg";
+
 import "./Footer.css";
 export default function Footer() {
     return (
@@ -84,6 +97,62 @@ export default function Footer() {
                         </a>
                     </p>
                 </form>
+                <h3>Мы в социальных сетях</h3>
+                <ul className="icons-container">
+                    <li>
+                        <a href="">
+                            <img src={yt_logo} alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src={insta_logo} alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src={facebook_logo} alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src={vk_logo} alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src={tg_logo} alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src={ok_logo} alt="" />
+                        </a>
+                    </li>
+                </ul>
+                <h3>Мы принимаем к оплате</h3>
+                <ul className="icons-container">
+                    <li>
+                        <a href="">
+                            <img src={visa} alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src={mastercard} alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src={mir} alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src={gpay} alt="" />
+                        </a>
+                    </li>
+                </ul>
             </div>
         </footer>
     );
