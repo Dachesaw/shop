85050ae65c0e4284b2a03599e8ec9ad2bbc9efe0
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-05-01 16:51:53 +0300

    add PopularCategory in componets

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,6 +1,7 @@
 import './MainPage.css'
 import CategotyMain from '../CategoryMain/CategoryMain'
 import Ads from '../Ads/Ads'
+import PopularCategory from '../PopularCategory/PopularCategory'
 
 import left from '../../assets/left-ads-button.svg'
 import right from '../../assets/right-ads-button.svg'
@@ -18,54 +19,7 @@ const MainPage = () => {
         <main>
             <CategotyMain/>
             <Ads/>
-            
-            <div className="popular-category">
-                <h2>Популярные категории</h2>
-                <div className='button-wrap'>
-                    <button id='buttons-ads-left-2' ><img src={left} alt="" /></button>
-                    <button id='buttons-ads-right-2' ><img src={right} alt="" /></button>
-                </div>
-                <div className="wrap-items-popular-cat">
-                    <figure>
-                        <img src={phone} alt="" />
-                        <figcaption>
-                            <a href="">Смартфоны</a>
-                        </figcaption>
-                    </figure>
-                    <figure>
-                        <img src={washing} alt="" />
-                        <figcaption>
-                            <a href="">Стиральные<br />машины</a>
-                        </figcaption>
-                    </figure>
-                    <figure>
-                        <img src={vacum} alt="" />
-                        <figcaption>
-                            <a href="">Пылесосы</a>
-                        </figcaption>
-                    </figure>
-                    <figure>
-                        <img src={fridge} alt="" width={60} />
-                        <figcaption>
-                            <a href="">Холодильники</a>
-                        </figcaption>
-                    </figure>
-                    <figure>
-                        <img src={tv} alt="" />
-                        <figcaption>
-                            <a href="">Телевизоры</a>
-                        </figcaption>
-                    </figure>
-                    <figure>
-                        <img src={microwave} alt="" />
-                        <figcaption>
-                            <a href="">Микроволновые<br />печи</a>
-                        </figcaption>
-                    </figure>
-                </div>
-            </div>
-            
-
+            <PopularCategory/>
         </main>
      );
 }
---
+++ src/components/PopularCategory/PopularCategory.tsx
@@ -0,0 +1,63 @@
+import './PopularCategory.css'
+import left from '../../assets/left-ads-button.svg'
+import right from '../../assets/right-ads-button.svg'
+
+import phone from '../../assets/phone.png'
+import washing from '../../assets/washing.png'
+import vacum from '../../assets/vacum.png'
+import fridge from '../../assets/fridge.png'
+import tv from '../../assets/tv.png'
+import microwave from '../../assets/microwave.png'
+
+const PopularCategory = () => {
+    return ( 
+        <div className="popular-category">
+                <h2>Популярные категории</h2>
+                <div className='button-wrap'>
+                    <button id='buttons-ads-left-2' ><img src={left} alt="" /></button>
+                    <button id='buttons-ads-right-2' ><img src={right} alt="" /></button>
+                </div>
+                <div className="wrap-items-popular-cat">
+                    <figure>
+                        <img src={phone} alt="" />
+                        <figcaption>
+                            <a href="">Смартфоны</a>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <img src={washing} alt="" />
+                        <figcaption>
+                            <a href="">Стиральные<br />машины</a>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <img src={vacum} alt="" />
+                        <figcaption>
+                            <a href="">Пылесосы</a>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <img src={fridge} alt="" width={60} />
+                        <figcaption>
+                            <a href="">Холодильники</a>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <img src={tv} alt="" />
+                        <figcaption>
+                            <a href="">Телевизоры</a>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <img src={microwave} alt="" />
+                        <figcaption>
+                            <a href="">Микроволновые<br />печи</a>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+
+     );
+}
+ 
+export default PopularCategory;
