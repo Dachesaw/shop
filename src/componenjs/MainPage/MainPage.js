44bbdb6335f34bbeb69547e0691c889319f97351
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-05-01 16:27:06 +0300

    add CategoryMain componets

--- src/componenjs/MainPage/MainPage.js
+++ src/componenjs/MainPage/MainPage.js
@@ -5,6 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 require("./MainPage.css");
+var _CategoryMain = _interopRequireDefault(require("../CategoryMain/CategoryMain"));
 var _leftAdsButton = _interopRequireDefault(require("../../assets/left-ads-button.svg"));
 var _rightAdsButton = _interopRequireDefault(require("../../assets/right-ads-button.svg"));
 var _phone = _interopRequireDefault(require("../../assets/phone.png"));
@@ -15,27 +16,7 @@ var _tv = _interopRequireDefault(require("../../assets/tv.png"));
 var _microwave = _interopRequireDefault(require("../../assets/microwave.png"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 var MainPage = function MainPage() {
-  return /*#__PURE__*/React.createElement("main", null, /*#__PURE__*/React.createElement("div", {
-    className: "category"
-  }, /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    href: ""
-  }, "\u0421\u043C\u0430\u0440\u0442\u0444\u043E\u043D\u044B")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    href: ""
-  }, "\u0421\u0442\u0438\u0440\u0430\u043B\u044C\u043D\u044B\u0435 \u043C\u0430\u0448\u0438\u043D\u044B")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    href: ""
-  }, "\u041F\u044B\u043B\u0435\u0441\u043E\u0441\u044B")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    href: ""
-  }, "\u0425\u043E\u043B\u043E\u0434\u0438\u043B\u044C\u043D\u0438\u043A\u0438")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    href: ""
-  }, "\u0422\u0435\u043B\u0435\u0432\u0438\u0437\u043E\u0440\u044B")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    href: ""
-  }, "\u041C\u0438\u043A\u0440\u043E\u0432\u043E\u043B\u043D\u043E\u0432\u044B\u0435 \u043F\u0435\u0447\u0438")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    href: ""
-  }, "\u041D\u0430\u0443\u0448\u043D\u0438\u043A\u0438")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    href: ""
-  }, "\u042D\u043B\u0435\u043A\u0442\u0440\u043E\u0447\u0430\u0439\u043D\u0438\u043A\u0438")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    href: ""
-  }, "\u0412\u044B\u0442\u044F\u0436\u043A\u0438")))), /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React.createElement("main", null, /*#__PURE__*/React.createElement(_CategoryMain.default, null), /*#__PURE__*/React.createElement("div", {
     className: "ads-wrap"
   }, /*#__PURE__*/React.createElement("div", {
     className: "button-wrap"
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,4 +1,6 @@
 import './MainPage.css'
+import CategotyMain from '../CategoryMain/CategoryMain'
+
 import left from '../../assets/left-ads-button.svg'
 import right from '../../assets/right-ads-button.svg'
 import phone from '../../assets/phone.png'
@@ -11,19 +13,7 @@ import microwave from '../../assets/microwave.png'
 const MainPage = () => {
     return ( 
         <main>
-            <div className="category">
-                <ul>
-                    <li><a href="">Смартфоны</a></li>
-                    <li><a href="">Стиральные машины</a></li>
-                    <li><a href="">Пылесосы</a></li>
-                    <li><a href="">Холодильники</a></li>
-                    <li><a href="">Телевизоры</a></li>
-                    <li><a href="">Микроволновые печи</a></li>
-                    <li><a href="">Наушники</a></li>
-                    <li><a href="">Электрочайники</a></li>
-                    <li><a href="">Вытяжки</a></li>
-                </ul>
-            </div>
+            <CategotyMain/>
             <div className="ads-wrap">
                 <div className='button-wrap'>
                     <button id='buttons-ads-left' ><img src={left} alt="" /></button>
