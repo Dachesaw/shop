e46d7e96b772de724031901eccff1ffa3860b5e8
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-05-02 20:51:21 +0300

    fix

--- src/components/PopularCategory/PopularCategory.tsx
+++ src/components/PopularCategory/PopularCategory.tsx
@@ -13,46 +13,46 @@ const PopularCategory = () => {
     return ( 
         <div className="popular-category">
                 <h2>Популярные категории</h2>
-                    <button className='buttons-ads-left' ><img src={left} alt="" /></button>
-                    <button className='buttons-ads-right' ><img src={right} alt="" /></button>
-                <div className="wrap-items-popular-cat">
-                    <figure>
-                        <img src={phone} alt="" />
-                        <figcaption>
-                            <a href="">Смартфоны</a>
-                        </figcaption>
-                    </figure>
-                    <figure>
-                        <img src={washing} alt="" />
-                        <figcaption>
-                            <a href="">Стиральные<br />машины</a>
-                        </figcaption>
-                    </figure>
-                    <figure>
-                        <img src={vacum} alt="" />
-                        <figcaption>
-                            <a href="">Пылесосы</a>
-                        </figcaption>
-                    </figure>
-                    <figure>
-                        <img src={fridge} alt="" width={60} />
-                        <figcaption>
-                            <a href="">Холодильники</a>
-                        </figcaption>
-                    </figure>
-                    <figure>
-                        <img src={tv} alt="" />
-                        <figcaption>
-                            <a href="">Телевизоры</a>
-                        </figcaption>
-                    </figure>
-                    <figure>
-                        <img src={microwave} alt="" />
-                        <figcaption>
-                            <a href="">Микроволновые<br />печи</a>
-                        </figcaption>
-                    </figure>
-                </div>
+                <button className='buttons-ads-left' ><img src={left} alt="" /></button>
+                        <button className='buttons-ads-right' ><img src={right} alt="" /></button>
+                    <div className="wrap-items-popular-cat">
+                        <figure>
+                            <img src={phone} alt="" />
+                            <figcaption>
+                                <a href="">Смартфоны</a>
+                            </figcaption>
+                        </figure>
+                        <figure>
+                            <img src={washing} alt="" />
+                            <figcaption>
+                                <a href="">Стиральные<br />машины</a>
+                            </figcaption>
+                        </figure>
+                        <figure>
+                            <img src={vacum} alt="" />
+                            <figcaption>
+                                <a href="">Пылесосы</a>
+                            </figcaption>
+                        </figure>
+                        <figure>
+                            <img src={fridge} alt="" width={60} />
+                            <figcaption>
+                                <a href="">Холодильники</a>
+                            </figcaption>
+                        </figure>
+                        <figure>
+                            <img src={tv} alt="" />
+                            <figcaption>
+                                <a href="">Телевизоры</a>
+                            </figcaption>
+                        </figure>
+                        <figure>
+                            <img src={microwave} alt="" />
+                            <figcaption>
+                                <a href="">Микроволновые<br />печи</a>
+                            </figcaption>
+                        </figure>
+                    </div>
             </div>
 
      );
