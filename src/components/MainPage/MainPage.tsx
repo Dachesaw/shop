f84e95cd5f0e37e0486e5d0a039622b47cd147ed
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-05-02 20:58:27 +0300

    remove import svg

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -4,17 +4,6 @@ import Ads from '../Ads/Ads'
 import PopularCategory from '../PopularCategory/PopularCategory'
 import BestSellers from '../BestSellers/BestSellers'
 
-import left from '../../assets/left-ads-button.svg'
-import right from '../../assets/right-ads-button.svg'
-
-import phone from '../../assets/phone.png'
-import washing from '../../assets/washing.png'
-import vacum from '../../assets/vacum.png'
-import fridge from '../../assets/fridge.png'
-import tv from '../../assets/tv.png'
-import microwave from '../../assets/microwave.png'
-
-
 const MainPage = () => {
     return ( 
         <main>
