86950f21c61f508daa5a6a4ae0b39cc4ea10957b
kinokorain <boriskoznev@gmail.com>
2024-05-03 21:37:30 +0300

    add React Router for Item page, add Link component in Card.tsx

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,22 +1,22 @@
-import './MainPage.css'
-import CategotyMain from '../CategoryMain/CategoryMain'
-import Ads from '../Ads/Ads'
-import PopularCategory from '../PopularCategory/PopularCategory'
-import BestSellers from '../BestSellers/BestSellers'
-import SeasonalProducts from '../SeasonalProducts/SeasonalProducts'
-import ServicesBlock from '../ServicesBlock/ServicesBlock'
+import "./MainPage.css";
+import CategotyMain from "../CategoryMain/CategoryMain";
+import Ads from "../Ads/Ads";
+import PopularCategory from "../PopularCategory/PopularCategory";
+import BestSellers from "../BestSellers/BestSellers";
+import SeasonalProducts from "../SeasonalProducts/SeasonalProducts";
+import ServicesBlock from "../ServicesBlock/ServicesBlock";
 
 const MainPage = () => {
-    return ( 
+    return (
         <main>
-            <CategotyMain/>
-            <Ads/>
-            <PopularCategory/>
+            <CategotyMain />
+            <Ads />
+            <PopularCategory />
             <BestSellers />
             <SeasonalProducts />
-            <ServicesBlock/>
+            <ServicesBlock />
         </main>
-     );
-}
- 
+    );
+};
+
 export default MainPage;
