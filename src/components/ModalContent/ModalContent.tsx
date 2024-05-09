df6765819896a819d69399ccb03aff7a5fe1d40d
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-05-09 13:29:45 +0300

    add html

---
+++ src/components/ModalContent/ModalContent.tsx
@@ -0,0 +1,16 @@
+const ModalContent = () => {
+    return ( 
+    <div className="modalcontent-wrap">
+        <div className="modalcontent">
+            <h3>Вход или регистрация</h3>
+            <p>Введите номер телефона. Мы отправим вам код в СМС.</p>
+            <form action="number">
+                <input type="tel" />
+                <input type="submit" value="Получить код" />
+            </form>
+        </div>
+    </div>
+    );
+}
+ 
+export default ModalContent;
