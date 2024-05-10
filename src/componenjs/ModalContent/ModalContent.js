574c7dcc0551289273edb6131bd804b65d359c4f
Dachesaw <123497362+Dachesaw@users.noreply.github.com>
2024-05-10 20:41:47 +0300

    -

---
+++ src/componenjs/ModalContent/ModalContent.js
@@ -0,0 +1,21 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var ModalContent = function ModalContent() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "modalcontent-wrap"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "modalcontent"
+  }, /*#__PURE__*/React.createElement("h3", null, "\u0412\u0445\u043E\u0434 \u0438\u043B\u0438 \u0440\u0435\u0433\u0438\u0441\u0442\u0440\u0430\u0446\u0438\u044F"), /*#__PURE__*/React.createElement("p", null, "\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u043D\u043E\u043C\u0435\u0440 \u0442\u0435\u043B\u0435\u0444\u043E\u043D\u0430. \u041C\u044B \u043E\u0442\u043F\u0440\u0430\u0432\u0438\u043C \u0432\u0430\u043C \u043A\u043E\u0434 \u0432 \u0421\u041C\u0421."), /*#__PURE__*/React.createElement("form", {
+    action: "number"
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "tel"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "submit",
+    value: "\u041F\u043E\u043B\u0443\u0447\u0438\u0442\u044C \u043A\u043E\u0434"
+  }))));
+};
+var _default = exports.default = ModalContent;
